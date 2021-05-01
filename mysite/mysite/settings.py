6399cd68f85a19ee9c0ab5d9d28f808dcb3edcc1
timeonator <timeonator@gmail.com>
2021-05-01 02:06:36 -0700

    added setup for postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,10 +74,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'mysite',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
